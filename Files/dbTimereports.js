55cbe47f916573a4fef8dc828c532e180b3015f5
Aldor B <vlad.ur@live.se>
2023-03-28 20:51:02 +0200

    Update dbTimereports.js

--- Files/dbTimereports.js
+++ Files/dbTimereports.js
@@ -1,29 +1,30 @@
-const dotenv = require("dotenv").config(); //dotenv för att spara vår token och databas id i appens "Enviourment".
-const { client, Client } = require("@notionhq/client"); //Ett js bibliotek som gör det lättare att koppla mot notion API
-
+const dotenv = require("dotenv").config();                    
+const {client, Client} = require("@notionhq/client");         
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
-const database_id3 = process.env.TIMEREPORTS_DB;
+const databaseTime = process.env.TIMEREPORTS_DB;
 
-module.exports = GetTimeReports = async () => {
-  // const test = await GetWork2();
-  // console.log(test)
-
+module.exports = GetTime = async() => {
+  
   const myDownload = {
-    path: "databases/" + database_id3 + "/query",
+   
+    path: "databases/" + databaseTime + "/query",
     method: "POST",
   };
-  const { results } = await notion.request(myDownload);
-  const timeReports = results.map((page) => {
+
+  const {results}=await notion.request(myDownload);
+  
+  const time = results.map((page)=>{
     return {
-      ProjectId: page.properties.Project.relation[0].id,
+      Projectid: page.properties.Project.relation[0].id,
+      Hours: page.properties.number,
       WorkedHours: page.properties.Hours.number,
-      StartDate: page.properties.Date.date.start,
-      EndDate: page.properties.Date.date.end,
-      PersonId: page.properties.Person.relation[0].id,
+      StartDate: page.properties.Date.date ? page.properties.Date.date.start : null,
+      EndDate: page.properties.Date.date ? page.properties.Date.date.start : null,
+      PersonId: page.properties.Person.relation,
       Url_comments: page.url,
-    };
+    }
   });
-  return timeReports;
+  return time;
 };
